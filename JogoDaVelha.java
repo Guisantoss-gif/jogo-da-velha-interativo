0ca0dd4879c8b16c7cdc8b002ca0723403f34460
Guisantoss-gif <go9104390@gmail.com>
2026-04-23 19:17:31 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,62 +1,53 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class JogoDaVelha
-{
-    public static void main(String args[])
-    {
+class JogoDaVelha {
+
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private Tabuleiro tabuleiro;
+
+    public JogoDaVelha(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
+        this.jogador1 = jogador1;
+        this.jogador2 = jogador2;
+        this.tabuleiro = tabuleiro;
+    }
+
+    private void iniciar() {
+
         Scanner sc = new Scanner(System.in);
-
-        Jogador jogador1, jogador2;
-
-        System.out.print("Escolha seu símbolo X ou O: ");
-        char simbolo = sc.next().toUpperCase().charAt(0);
-
-        char simbolo2 = (simbolo == 'X') ? 'O' : 'X';
-
-        jogador1 = new Jogador(simbolo);   
-        jogador2 = new Jogador(simbolo2);  
-        
-        Tabuleiro tabuleiro = new Tabuleiro();
-
-        int linha, coluna;
         Random random = new Random();
 
+        int linha, coluna;
+        
         int partida = 1;
 
-        while(!tabuleiro.acabouOJogo())
-        {
+        while (!this.tabuleiro.acabouOJogo()) {
             System.out.println("Partida " + partida);
 
-            
-            System.out.println("Jogador 1 (" + jogador1.getSimbolo() + ")");
+            System.out.println("Jogador 1 (" + this.jogador1.getSimbolo() + ")");
             do {
                 System.out.print("Linha (0-2): ");
                 linha = sc.nextInt();
                 System.out.print("Coluna (0-2): ");
                 coluna = sc.nextInt();
-            }    
-            while (!tabuleiro.jogar(jogador1.getSimbolo(),linha,coluna));
+            } while (!this.tabuleiro.jogar(this.jogador1.getSimbolo(), linha, coluna));
 
-            if (tabuleiro.acabouOJogo())
-            {
+            if (tabuleiro.acabouOJogo()) {
                 break;
             }
 
-           
-            System.out.println("Jogador 2 (" + jogador2.getSimbolo() + ")");
+            System.out.println("Jogador 2 (" + this.jogador2.getSimbolo() + ")");
             do {
-                linha = random.nextInt(0,3);
-                coluna = random.nextInt(0,3);
-            }    
-            while(!tabuleiro.jogar(jogador2.getSimbolo(),linha,coluna));
+                linha = random.nextInt(0, 3);
+                coluna = random.nextInt(0, 3);
+            } while (!this.tabuleiro.jogar(this.jogador2.getSimbolo(), linha, coluna));
 
             partida++;
         }
 
-       
-        if (tabuleiro.haUmVencedor()) {
-            if (tabuleiro.getUltimoSimbolo() == jogador1.getSimbolo()) {
+        if (this.tabuleiro.haUmVencedor()) {
+            if (this.tabuleiro.getUltimoSimbolo() == this.jogador1.getSimbolo()) {
                 System.out.println("O jogador 1 ganhou");
             } else {
                 System.out.println("O jogador 2 ganhou");
@@ -66,5 +57,24 @@ class JogoDaVelha
         }
 
         System.out.println("Criado por Guilherme Oliveira Santos");
+    };
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        Jogador jogador1, jogador2;
+
+        System.out.print("Escolha seu símbolo X ou O: ");
+        char simbolo = sc.next().toUpperCase().charAt(0);
+
+        char simbolo2 = (simbolo == 'X') ? 'O' : 'X';
+
+        jogador1 = new Jogador(simbolo);
+        jogador2 = new Jogador(simbolo2);
+
+        Tabuleiro tabuleiro = new Tabuleiro();
+        JogoDaVelha jogo = new JogoDaVelha(jogador1, jogador2, tabuleiro);
+        jogo.iniciar();
+
     }
 }
